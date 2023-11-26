9a0cf76506f811973ccccedb4ba648698bd9a704
ruficarl <107566254+flaucirr@users.noreply.github.com>
2023-11-26 22:20:16 +0800

    organized files

---
+++ Database 1.0/clientdatabase_user_personal.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clientdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_personal`
+--
+
+DROP TABLE IF EXISTS `user_personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_personal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) NOT NULL,
+  `f_name` varchar(255) NOT NULL,
+  `m_name` varchar(255) NOT NULL,
+  `l_name` varchar(255) NOT NULL,
+  `suffix` varchar(45) DEFAULT NULL,
+  `sex_id` int DEFAULT NULL,
+  `cvl_id` int DEFAULT NULL,
+  `b_date` date DEFAULT NULL,
+  `b_place` varchar(255) DEFAULT NULL,
+  `res_id` int DEFAULT NULL,
+  `czn_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_personal`
+--
+
+LOCK TABLES `user_personal` WRITE;
+/*!40000 ALTER TABLE `user_personal` DISABLE KEYS */;
+INSERT INTO `user_personal` VALUES (1,'MM3284','Mhandee Louise','Bautista','Matis',NULL,2,2,NULL,NULL,NULL,NULL),(2,'RL1741','Rufi Carl','Prudenciado','Lagaras',NULL,2,2,NULL,'Navotas',NULL,NULL);
+/*!40000 ALTER TABLE `user_personal` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-18 23:16:14
